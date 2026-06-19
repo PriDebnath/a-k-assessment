0ee52d93d4c00c037e37f40b7090124d3da25522
Pritam <debnathpritam0802@gmail.com>
2026-06-19 15:16:44 +0530

    added meta data for the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,28 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const title = "Student Dashboard"
+const description = "Next-gen learning dashboard"
+
 export const metadata: Metadata = {
-  title: "Student Dashboard",
-  description: "Next-gen learning dashboard",
+  title,
+  description,
+  authors: {
+    name: "Pritam Debnath",
+    url: "https://github.com/PriDebnath/portfolio-v2"
+  },
+  openGraph: {
+    title,
+    description,
+    images: [
+      {
+        url: "https://raw.githubusercontent.com/PriDebnath/a-k-assessment/refs/heads/main/public/images/app-demo-desktop-19-06-2026.png",
+        width: 1200,
+        height: 630,
+        alt: "Preview image",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
